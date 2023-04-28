8b82cfa411de084950fbae2347a93b4d09e1790f
emfig <emfigueroa0630@gmail.com>
2023-04-28 01:52:00 -0400

    BASIC JAVA ASIGN

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,5 @@
+window.alert("Welcome") //WELCOME MESSAGE DROPDOWN//
+var Sent1 = "This is the beginning of the string", Sent2 = "and this is the end of the string" //VARIABLE ASSIGNMENTS//
+document.write(Sent1 + " " + Sent2) //VARIABLE CONCATENATION//
+document.write("<br>") //BREAK TO SEPERATE EXPRESSION AND STRING//
+document.write(5 + 5)  //EXPRESSION//
